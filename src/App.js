daa5321bcd5aeca3e25f64c9292301807254baba
dgrizzla <dylandau5@gmail.com>
2016-10-26 22:17:16 -0600

    se agrego bootstrap y se hicieron modificaciones del diseño

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import React from 'react';
-import * as firebase from 'firebase';
 import './App.css';
-
-const provider = new firebase.auth.GoogleAuthProvider();
+import './vendor/Bootstrap/css/bootstrap.css';
+import SignIn from './SignIn.js';
 
 const obj = {};
 
 obj.displayName = 'App';
 
 obj.render = function () {
-  return <div>
-    <h1>{this.state.name}</h1>
-    <button onClick={this.onClick}>sign in</button> 
+  return <div>    
+    <SignIn></SignIn> 
   </div>;
 };
-
-obj.componentDidMount = function () {
-  const nameRef = firebase.database().ref().child('Object').child('name');
-  nameRef.on('value', snapshot => {
-    this.setState({
-      name: snapshot.val()
-    });
-  });
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      console.log('Yes', user);
-    } else {
-      console.log('No');
-    }
-  });
-};
-
-obj.onClick = function (evt) {
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
-    console.log(token, user);
-  }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    console.log(errorCode, errorCode, errorMessage, email, credential);
-  });
-};
-
-obj.getInitialState = function () {
-  return {};
-};
-
 
 const App = React.createClass(obj);
 
---
+++ src/SignIn.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import * as firebase from 'firebase';
+
+const provider = new firebase.auth.GoogleAuthProvider();
+const obj = {};
+
+obj.render = function () {
+  return <div>
+    <h1 className="titulo text-center">Peek</h1>
+    <div className="well center-block sign-in-module">
+      <button type="button" className="btn btn-success btn-block " onClick={this.onClick}>Sign In</button>
+    </div>
+  </div>;
+};
+
+obj.componentDidMount = function () {
+  const nameRef = firebase.database().ref().child('Object').child('name');
+  nameRef.on('value', snapshot => {
+    this.setState({
+      name: snapshot.val()
+    });
+  });
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('Yes', user);
+    } else {
+      console.log('No');
+    }
+  });
+};
+
+obj.onClick = function (evt) {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+    console.log(token, user);
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    console.log(errorCode, errorCode, errorMessage, email, credential);
+  });
+};
+
+obj.getInitialState = function () {
+  return {};
+};
+
+const SignIn = React.createClass(obj);
+
+export default SignIn;
